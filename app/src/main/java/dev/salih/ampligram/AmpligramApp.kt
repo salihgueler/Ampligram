7fc69e939e69b672fd149676d643be37c8125264
salihgueler <muhammedsalihguler@gmail.com>
2023-04-11 23:41:00 +0200

    Add the initialization result.

--- app/src/main/java/dev/salih/ampligram/AmpligramApp.kt
+++ app/src/main/java/dev/salih/ampligram/AmpligramApp.kt
@@ -1,7 +1,20 @@
 package dev.salih.ampligram
 
 import android.app.Application
+import android.util.Log
+import com.amplifyframework.AmplifyException
+import com.amplifyframework.core.Amplify
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class AmpligramApp: Application()
+class AmpligramApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        try {
+            Amplify.configure(applicationContext)
+            Log.i("Ampligram", "Initialized Amplify")
+        } catch (error: AmplifyException) {
+            Log.e("Ampligram", "Could not initialize Amplify", error)
+        }
+    }
+}
